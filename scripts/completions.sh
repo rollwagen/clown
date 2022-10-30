99bb8896ac2a9d63e6e8d69a9a9faa842ac5585c
rollwagen <rollwagen@gmail.com>
2022-10-30 18:18:24 +0100

    fix: goreleaser config add archive

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,6 +3,6 @@ CLI_NAME="clown"
 set -e
 rm -rf completions
 mkdir completions
-for sh in bash zsh fish; do
+for sh in "bash" "zsh" "fish"; do
 	go run main.go completion "$sh" >"completions/$CLI_NAME.$sh"
 done
